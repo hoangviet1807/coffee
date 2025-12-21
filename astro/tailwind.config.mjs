cd403f78a70dcbc3aea78b9cccf33cf40f6157bf
bnhviet <hoangviet1807@gmail.com>
2025-12-21 14:06:27 +0700

    refactor structure and add sanity

---
+++ astro/tailwind.config.mjs
@@ -0,0 +1,15 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+	content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue,tsx}'],
+	plugins: [],
+	theme: {
+    extend: {
+      fontFamily: {
+        sans: ['Poppins', 'ui-sans-serif', 'system-ui'], // Poppins as default sans
+      },
+    },
+      backgroundImage: {
+      'banner': "url('./public/banner.jpg')",
+    },
+  },
+}
